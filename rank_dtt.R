1cfcfbb9e2fc9536e296141fe0e86722921191ac
David Murrell <d.murrell@ucl.ac.uk>
2017-12-20 09:19:33 +0000

    Create rank_dtt.R
    This function takes an object produced by the dtt function of geiger and turns it into an object that can be run through the rank envelope test function

---
+++ rank_dtt.R
@@ -0,0 +1,32 @@
+####################################################
+#
+#	Generic function to compute rank envelope test
+#
+####################################################
+
+rank_env_dtt<-function(x, Plot=T, test="two.sided")
+{
+		
+	spp_num<-length(x$times)		
+	sims<-x$sim
+	sims<-as.matrix(sims)
+
+	s1<-sims[-c(1),]
+	
+	r<-x$times[-c(1)]
+
+	r<-as.vector(r)
+
+	obs<-as.vector(x$dtt)
+	obs<-obs[-c(1)]
+
+	c1<-list(r,obs, s1)
+	names(c1)=c("r","obs","sim_m") 
+	c2<-create_curve_set(c1)
+
+	res<-rank_envelope(c2, alternative=test)
+	
+	if(Plot==T)
+	plot(res, xlab="Relative time", ylab="Disparity", main="")	
+	return(res)	
+}
